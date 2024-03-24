664fed4b84113eec8ffefc12c7c636fca24a1195
Alvin <alvinksabu200115@gmail.com>
2024-03-24 17:17:25 +0530

    all_done

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,54 @@ from django.shortcuts import get_object_or_404, redirect, render
 from .models import Post,Comment
 from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
-
+from django.contrib import messages
+from django.shortcuts import redirect
 
 from django.utils import timezone
+
+#new views
+
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from django.shortcuts import render, redirect
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
+
+from .forms import RegistrationForm
+
+
+
+from django.contrib import messages
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Registration successful. Please log in.')
+            return redirect('login')  # Redirect to login page after successful registration
+    else:
+        form = RegistrationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('post_list')  # Redirect to post list page after successful login
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('post_list')
+
 
 
 # Create your views here.
@@ -15,9 +60,11 @@ def post_list(request):
 
 
 
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    
     return render(request, 'blog/post_detail.html', {'post': post})
 
 
